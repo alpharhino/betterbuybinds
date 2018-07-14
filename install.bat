576746c539fb20fccc584acfa3558a2f8dff84cd
Ryan Fisher <alpharyano@gmail.com>
2018-07-14 00:18:38 -0500

    Fixed auto install script to check if folder exists before writing to it.
    Signed-off-by: Ryan Fisher <alpharyano@gmail.com>

--- install.bat
+++ install.bat
@@ -1,11 +1,23 @@
-@echo off
+@ECHO off
+setlocal EnableDelayedExpansion
 SET GAMEDIR="C:\Program Files (x86)\steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\"
 SET AUTOEXECFILE=autoexec.cfg
-echo %AUTOEXECFILE%
-if exist %GAMEDIR% echo folder exists
-echo WARNING, THIS WILL OVERWRITE YOUR AUTOEXEC CONFIG IN
-echo %GAMEDIR%
-SET /P AREYOUSURE=Are you sure (Y/[N])? 
-IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
-copy %AUTOEXECFILE% %GAMEDIR% /Y
-pause
+IF NOT EXIST %GAMEDIR% (
+    ECHO ERROR %GAMEDIR% does not exist! 
+	ECHO Autoexec will not be installed.
+	ECHO Exiting...
+	GOTO ERROREXIT
+) ELSE (
+    echo WARNING: THIS WILL OVERWRITE YOUR AUTOEXEC CONFIG IN
+	echo %GAMEDIR%
+	SET /P AREYOUSURE=Are you sure (Y/[N]^)^? 
+	IF /I "!AREYOUSURE!" NEQ "Y" GOTO QUIT
+	copy %AUTOEXECFILE% %GAMEDIR% /Y
+	pause
+)
+
+:ERROREXIT
+EXIT /B 1
+
+:QUIT
+EXIT /B 0
