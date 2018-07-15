7035a7ee7360298860a1047be3a1ea3950d6987c
Ryan Fisher <alpharyano@gmail.com>
2018-07-15 15:36:01 -0500

    Moved config files into new folder so we copy folder contents into csgo/cfg. Continued development on python script for install.

--- install.bat
+++ install.bat
@@ -1,23 +1,28 @@
 @ECHO off
 setlocal EnableDelayedExpansion
 SET GAMEDIR="C:\Program Files (x86)\steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\"
-SET AUTOEXECFILE=autoexec.cfg
+SET CFGFILES=cfg
+REM Check if the game is installed in the default location. We'd like to make this better at finding the correct directory in the future
 IF NOT EXIST %GAMEDIR% (
+	REM If we don't find the install directory, fail and notify the user
     ECHO ERROR %GAMEDIR% does not exist! 
+	ECHO You may be seeing this because your install location is not default, or your game is not installed.
 	ECHO Autoexec will not be installed.
 	ECHO Exiting...
 	GOTO ERROREXIT
 ) ELSE (
-    echo WARNING: THIS WILL OVERWRITE YOUR AUTOEXEC CONFIG IN
-	echo %GAMEDIR%
+	REM Ask user to confirm installation
+	echo Confirm installation of %CFGFILES% into %GAMEDIR%
 	SET /P AREYOUSURE=Are you sure (Y/[N]^)^? 
 	IF /I "!AREYOUSURE!" NEQ "Y" GOTO QUIT
-	copy %AUTOEXECFILE% %GAMEDIR% /Y
-	pause
+	REM Make directory and copy the files over into it
+	xcopy %CFGFILES% %GAMEDIR% /Y /Q
 )
 
 :ERROREXIT
+pause
 EXIT /B 1
 
 :QUIT
+pause
 EXIT /B 0
--- install.py
+++ install.py
@@ -1,10 +1,35 @@
 import os
+import sys
 
 
 def main():
+    os_str = sys.platform
+    if os_str == 'win32' or os_str == 'cygwin':
+        setup_win()
+    elif os_str == 'linux2':
+        setup_linux()
+    elif os_str == 'darwin':
+        setup_mac()
+
+
+def setup_win():
+    drive_letter = None
+    install_path = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\'
+    while drive_letter is None:
+        drive_letter = raw_input("Please enter the drive letter on which csgo is installed: ")
+        if len(drive_letter) != 1 or not drive_letter.isalpha():
+            drive_letter = None
+        if drive_letter.lower() == 'c'
+
+    print drive_letter
+
+
+def setup_linux():
     pass
 
 
-def get_user_input(str=None):
-    if str:
-        print str
+def setup_mac():
+    pass
+
+if __name__ == '__main__':  # Boilerplate start function
+    main()
